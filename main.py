a064b7205199356fe7287d3cbc330303a6a25494
tivenide <tivenide@gmail.com>
2023-03-28 23:06:16 +0200

    print_hi()

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Hi, {name}, Welcome to spikedetector')
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Spike interested data engineer')
 
 #for one recording
-path='data/'
+path='data/recording.h5'
 
 from tools import preprocessing_for_one_recording
-preprocessing_for_one_recording(path)
+#preprocessing_for_one_recording(path)
 
 from visualization import visualize_assignments_of_one_recording
-visualize_assignments_of_one_recording(path, 100)
+#visualize_assignments_of_one_recording(path, 100)
 
 print('finish')
