16533d2a01ff0507d707dcd21af722390e3b41f7
tivenide <tivenide@gmail.com>
2023-03-28 22:57:52 +0200

    application of preprocessing and visualization in main

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+#for one recording
+path='data/'
+
+from tools import preprocessing_for_one_recording
+preprocessing_for_one_recording(path)
+
+from visualization import visualize_assignments_of_one_recording
+visualize_assignments_of_one_recording(path, 100)
+
+print('finish')
