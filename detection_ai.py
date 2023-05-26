acf7b2b2f6c38ef6dab76ee2adcb471890c349f9
tivenide <tivenide@gmail.com>
2023-05-26 13:45:57 +0200

    added windowing_electrode_data()

--- detection_ai.py
+++ detection_ai.py
@@ -1,13 +1,18 @@
 # detection ai based
 
-def windowing_electrode_data(electrode_data, timestamps):
-    pass
+def windowing_electrode_data(electrode_data, timestamps, window_size=20):
+    electrode_data_windowed = []
+    timestamps_windowed = []
+    for i in range(0, len(electrode_data) - window_size + 1, window_size):
+        electrode_data_windowed.append(electrode_data[i:i + window_size])
+        timestamps_windowed.append(timestamps[i:i + window_size])
+    return electrode_data_windowed, timestamps_windowed
 
 import torch
 
 def function1(window):
     # Assuming `model` is your trained PyTorch model
-    model = torch.load('path_to_saved_model.pth')
+    model = torch.load('TransformerModel.pth')
     model.eval()
 
     # Preprocess the window data (if needed) and convert it to a PyTorch tensor
@@ -57,12 +62,12 @@ path_to_data_file_h5 = ''
 from tools import import_recording_h5
 signal_raw, timestamps, ground_truth, channel_positions, template_locations = import_recording_h5(path_to_data_file_h5)
 
-from detection_classical import preprocessing_for_one_recording_without_windowing, calculate_metrics_with_windows, plot_roc
+#from detection_classical import preprocessing_for_one_recording_without_windowing, calculate_metrics_with_windows, plot_roc
 
 detection_output = application_of_model(signal_raw, timestamps)
 
 # evaluation
-data_ground_truth = preprocessing_for_one_recording_without_windowing(path_to_data_file_h5)
+#data_ground_truth = preprocessing_for_one_recording_without_windowing(path_to_data_file_h5)
 
-accuracy_scores, precision_scores, recall_scores, f1_scores, _ = calculate_metrics_with_windows(data_ground_truth, detection_output, window_size=40)
-plot_roc(data_ground_truth, detection_output, window_size=40)
+#accuracy_scores, precision_scores, recall_scores, f1_scores, _ = calculate_metrics_with_windows(data_ground_truth, detection_output, window_size=40)
+#plot_roc(data_ground_truth, detection_output, window_size=40)
